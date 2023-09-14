f104ee436268031a0ba3548a9af15981398b027b
CamilaGiraldoGiraldo <giraldocamila2004@gmail.com>
2023-09-13 22:44:12 -0500

    INSERTAR TIENE ERROR

--- src/Polinomio2.java
+++ src/Polinomio2.java
@@ -106,24 +106,22 @@ public class Polinomio2 {
 
     void SumaF2(Polinomio2 Poli) {
         int t = vector[0] + Poli.vector[0], k = 1, i = 2;
-        int[] Aux = new int[Poli.getDu() + 1];
-        Aux = getVector();
         Polinomio2 suma = new Polinomio2(t);
-        while (i < vector.length && i < Aux.length) {
-            if (vector[i] > Aux[i]) {
+        while (i < vector.length && i < Poli.vector.length) {
+            if (vector[i] > Poli.vector[i]) {
                 suma.vector[k] = vector[i - 1];
                 k++;
                 suma.vector[k] = vector[i];
                 k++;
-            } else if (Aux[i] > vector[i]) {
-                suma.vector[k] = Aux[i - 1];
+            } else if (Poli.vector[i] > vector[i]) {
+                suma.vector[k] = Poli.vector[i - 1];
                 k++;
-                suma.vector[k] = Aux[i];
+                suma.vector[k] = Poli.vector[i];
                 k++;
-            } else if (Aux[i] == vector[i]) {
-                suma.vector[k] = Aux[i - 1] + vector[i - 1];
+            } else if (Poli.vector[i] == vector[i]) {
+                suma.vector[k] = Poli.vector[i - 1] + vector[i - 1];
                 k++;
-                suma.vector[k] = Aux[i];
+                suma.vector[k] = Poli.vector[i];
                 k++;
             }
             i = i + 2;
@@ -137,20 +135,20 @@ public class Polinomio2 {
                 i++;
             }
         }
-        if (i < Aux.length) {
-            while (i < Aux.length) {
-                suma.vector[k] = Aux[i - 1];
+        if (i < Poli.vector.length) {
+            while (i < Poli.vector.length) {
+                suma.vector[k] = Poli.vector[i - 1];
                 k++;
-                suma.vector[k] = Aux[i];
+                suma.vector[k] = Poli.vector[i];
                 k++;
                 i++;
             }
         }
         int cont = 0;
-        i = suma.du - 1;
-        while (i <= 0 && suma.vector[i] == 0 && suma.vector[i - 1] == 0) {
-            cont++;
-            i--;
+        for (i = 2; i < suma.vector.length; i = i + 2) {
+            if (suma.vector[i - 1] != 0) {
+                cont = cont + 1;
+            }
         }
         suma.Redimensionar(cont);
 
@@ -215,23 +213,31 @@ public class Polinomio2 {
                         + Poli.MostrarNatural2() + "\n   *** La Resta es: ***   \n" + resta.MostrarNatural2());
 
     }
+
     void Redimensionar(int t) {
         int[] nuevo;
-        int i = 1;
-        nuevo = new int[du - t];
-        while (i < du) {
-            nuevo[i] = vector[i];
-            i++;
-        }
+        int i = 1, k = 1;
+        nuevo = new int[t * 2 + 1];
+        nuevo[0] = t;
+        while (i < t * 2 + 1) {
+            if (i % 2 != 0 && vector[k] == 0) {
+                k = k + 2;
+            } else {
+                nuevo[i] = vector[k];
+                i++;
+                k++;
+            }
 
+        }
+        vector = nuevo;
     }
-    
-    void EvaluarF2 (){
+
+    void EvaluarF2() {
         int x = 0, coeficiente = 0, Exponente = 0, Resultado = 0;
         x = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el valor que tendrá la X"));
-        for (int i = 1; i <= du; i=i+2) {
+        for (int i = 1; i <= du; i = i + 2) {
             coeficiente = vector[i];
-            Exponente = vector[i+1];
+            Exponente = vector[i + 1];
             Resultado = Resultado + (coeficiente * Potencia(x, Exponente));
         }
         JOptionPane.showMessageDialog(null, "   *** El polinomio Es:***  \n " + MostrarNatural2() + "\nLa ´X´ Es: " + x
@@ -249,4 +255,66 @@ public class Polinomio2 {
 
     }
 
+    void EliminarF2() {
+        int Exponente = 0, i = 2;
+        boolean Encontrado = false;
+        Exponente = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el exponente que desea eliminar"));
+        for (i = 2; i < du || vector[i] == Exponente; i = i + 2) {
+            if (vector[i] == Exponente) {
+                vector[i - 1] = 0;
+                vector[i] = 0;
+                Encontrado = true;
+            }
+        }
+        if (Encontrado) {
+            int cont = vector[0] - 1;
+            Redimensionar(cont);
+        } else {
+            JOptionPane.showMessageDialog(null, "   *** Coeficiente no encontrado***  \n ");
+        }
+        JOptionPane.showMessageDialog(null, "   *** Coeficiente eliminado con exito***  \n ");
+    }
+
+    Polinomio2 Insertar(int[] vec) {
+        Polinomio2 nuevo = new Polinomio2(vec);
+        int g,Posicion;
+        int exponente=nuevo.vector[2];
+        boolean encontrado=false;
+        for(int i=2; i<vector.length;i=i+2){
+            if (exponente==vector[i]){
+                encontrado=true;
+            }
+        }
+        if (encontrado==true) {
+            g = vector[0];
+        } else {
+            g = vector[0]+1;
+        }
+        Polinomio2 todo = new Polinomio2(g);
+        for (int i = 1; i < nuevo.vector.length; i = i + 2) {
+            Posicion = (g*2+1)- 1 - nuevo.vector[i];
+            if (nuevo.vector[i] == 0) {
+                todo.vector[Posicion] = todo.vector[Posicion] + nuevo.vector[i - 1];
+                Posicion = Posicion + 1;
+                todo.vector[Posicion] = nuevo.vector[i];
+            } else {
+                todo.vector[Posicion - 1] = todo.vector[Posicion - 1] + nuevo.vector[i - 1];
+                todo.vector[Posicion] = nuevo.vector[i];
+            }
+        }
+        for (int i = 1; i < vec.length; i = i + 2) {
+             Posicion = (g*2+1)- 1 - vector[i];
+            if (nuevo.vector[i] == 0) {
+                todo.vector[Posicion] = todo.vector[Posicion] + vector[i - 1];
+                Posicion = Posicion + 1;
+                todo.vector[Posicion] = vector[i];
+            } else {
+                todo.vector[Posicion - 1] = todo.vector[Posicion - 1] + vector[i - 1];
+                todo.vector[Posicion] = vector[i];
+            }
+        
+        }
+        return todo;
+
+    }
 }
